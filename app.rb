ef5c6bf98b5e8734256f9d020789883035754fd6
Brandon Weiss <brandon@anti-pattern.com>
2009-12-29 23:35:01 -0600

    Reworked iPhone detection

--- app.rb
+++ app.rb
@@ -2,9 +2,11 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
+  @stylesheet = @env['HTTP_USER_AGENT'].match(/iPhone/) ? 'iphone.css' : 'master.css'
+
   birthday = Date.strptime("1985-02-04")
-	@age = (Date.today - birthday).to_i / 365
-	
+  @age = (Date.today - birthday).to_i / 365
+  	
   erb :layout
 end
 
