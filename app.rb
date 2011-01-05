8fa4eaf8fb0db091852bbcfa25aae1b232588824
Brandon Weiss <brandon@anti-pattern.com>
2011-01-05 01:53:14 -0600

    Adding a root path

--- app.rb
+++ app.rb
@@ -3,6 +3,8 @@
 # end
 
 class App < Sinatra::Base
+  set :root, File.dirname(__FILE__)
+
   get '/' do
     birthday = Date.strptime('1985-02-04', '%F')
     @age = (Date.today - birthday).to_i / 365
