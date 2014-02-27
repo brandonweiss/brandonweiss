c46b9b39946bdb077cb4ef04983f62dac0a56ed4
Brandon Weiss <brandon@anti-pattern.com>
2014-02-26 16:37:15 -0800

    Storing the latest blog post in the repo as a file rather than fetching during precompilation

--- Rakefile
+++ Rakefile
@@ -15,3 +15,21 @@ desc "Remove compiled files"
 task :clean do
   sh "rm -rf #{File.dirname(__FILE__)}/build/*"
 end
+
+desc "Update blog post"
+task :update_blog_post do
+  require "httparty"
+
+  response    = HTTParty.get("http://anti-pattern.com/feed")
+  latest_post = response.parsed_response["feed"]["entry"].first
+
+  post = {
+    title: latest_post["title"],
+    date:  latest_post["updated"],
+    body:  latest_post["content"]["__content__"]
+  }
+
+  File.open("./source/latest_post.json", "w") do |file|
+    file.write(post.to_json)
+  end
+end
--- config.rb
+++ config.rb
@@ -1,28 +1,15 @@
-require "httparty"
-
 class LatestPost < Middleman::Extension
 
   def initialize(app, options_hash = {}, &block)
     super
 
     app.before do
-      if app.environment == :production
-        response    = HTTParty.get("http://anti-pattern.com/feed")
-        latest_post = response.parsed_response["feed"]["entry"].first
-      else
-        require "active_support/core_ext/hash/indifferent_access"
-        require "time"
+      file = File.read("./source/latest_post.json")
+      post = JSON.load(file)
 
-        latest_post = {
-          title:   "Test test test test test",
-          updated: Time.now.utc.iso8601,
-          content: { "__content__" => "Lorem ipsum. " * 50 }
-        }.with_indifferent_access
-      end
-
-      app.set :latest_post_title, latest_post["title"]
-      app.set :latest_post_date,  Date.parse(latest_post["updated"]).strftime("%B %d, %Y")
-      app.set :latest_post_body,  latest_post["content"]["__content__"]
+      app.set :latest_post_title, post["title"]
+      app.set :latest_post_date,  Date.parse(post["date"]).strftime("%B %d, %Y")
+      app.set :latest_post_body,  post["body"]
     end
   end
   # alias_method :included, :registered
