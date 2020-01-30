ce407225e3c3bcf40ebe441df2721bf432931152
Brandon Weiss <brandon@anti-pattern.com>
2020-01-30 14:11:36 -0800

    Switch from Gauges to Fathom

--- source/components/tracking.html.jsx
+++ source/components/tracking.html.jsx
@@ -1,29 +1,27 @@
-export default ({ environment } = props) => {
-  let isProduction = (environment === "production")
+export default ({ environment }) => {
+  let isProduction = environment === "production"
 
   let scriptBody = `
-    var _gauges = _gauges || [];
-    (function() {
-      var t   = document.createElement('script');
-      t.type  = 'text/javascript';
-      t.async = true;
-      t.id    = 'gauges-tracker';
-      t.setAttribute('data-site-id', '4d63cbd0a219863d81000002');
-      t.src = '//secure.gaug.es/track.js';
-      var s = document.getElementsByTagName('script')[0];
-      s.parentNode.insertBefore(t, s);
-    })();
+    (function(f, a, t, h, o, m){
+    a[h]=a[h]||function(){
+    (a[h].q=a[h].q||[]).push(arguments)
+    };
+    o=f.createElement('script'),
+    m=f.getElementsByTagName('script')[0];
+    o.async=1; o.src=t; o.id='fathom-script';
+    m.parentNode.insertBefore(o,m)
+    })(document, window, 'https://cdn.usefathom.com/tracker.js', 'fathom');
+    fathom('set', 'siteId', 'PIWFBROQ');
+    fathom('trackPageview');
   `
 
   let script = {
-    __html: scriptBody
+    __html: scriptBody,
   }
 
   if (isProduction) {
-    return (
-      <script type="text/javascript" dangerouslySetInnerHTML={script} />
-    )
+    return <script type="text/javascript" dangerouslySetInnerHTML={script} />
   } else {
-    return (null)
+    return null
   }
 }
